a3355ee34ae13f6cb13e2f7578dde01456031b0a
davidyap <davidyapsy@gmail.com>
2025-06-11 20:52:52 +0800

    remove travel app

--- TravelApp/src/main/java/com/tttravel/TravelApp/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.tttravel.TravelApp.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-	public ResourceNotFoundException(String msg) {
-		super(msg);
-	}
-}
